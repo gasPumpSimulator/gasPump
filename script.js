361953d7ddae06012bcebbb5a48253f528b3793f
pdurfey <3durfey@gmail.com>
2023-02-04 10:26:48 -0500

    added keypad and colors

---
+++ script.js
@@ -0,0 +1,15 @@
+    
+function changeColor(input)
+{
+    document.getElementById(input).style.backgroundColor = 'red';
+
+    if(document.getElementById('87Gas').style.backgroundColor === 'red');
+    {
+        return;
+    }
+
+    document.getElementById(input).style.backgroundColor = 'red';
+    document.getElementById('89Gas').style.backgroundColor = 'red';
+    document.getElementById('93Gas').style.backgroundColor = 'red';
+    document.getElementById('dieselGas').style.backgroundColor = 'red';
+}
