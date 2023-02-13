501123d24d7a636f085d1cf38d08a5626e477c5e
pdurfey <3durfey@gmail.com>
2023-02-13 16:30:19 -0500

    updated api and server

---
+++ gasPriceScraperApi.js
@@ -0,0 +1,17 @@
+import gotScraping from 'got-scraping';
+import cheerio from 'cheerio';
+
+const storeUrl = 'https://api.apify.com/v2/datasets/oRfOLi79S4TcYn7V7/items?token=apify_api_DVgZweSpe7Lp9jEr9AQht1ERabqQER1sxBgR';
+
+// Download HTML with Got Scraping
+const response = await gotScraping.gotScraping(storeUrl);
+const html = response.body;
+
+// Parse HTML with Cheerio
+const $ = cheerio.load(html);
+const headingElement = $('gasPrices');
+const headingText = headingElement.text();
+const jsonFile = JSON.parse(html);
+let dataSet = jsonFile[9];
+// Print page title to terminal
+console.log(dataSet.gasPrices[0].priceTag);
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const path = require('path')
+import express from 'express'
+import path from 'path'
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express()
 const port = 9000
-app.use(express.static(path.join(__dirname, 'public')))
 
+app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (request, response) => {
-  response.sendFile(path.join(__dirname, 'public/index.html'))
+  response.sendFile(path.join(__dirname, 'htmlCss/index.html'))
 })
 
 app.listen(port, () => {
