25c5d636782b33f26f70e559d95f3605e1aa0bde
pdurfey <3durfey@gmail.com>
2023-02-13 13:24:48 -0500

    added server and public folder

---
+++ public/script.js
@@ -0,0 +1,23 @@
+    
+function changeColor(input)
+{
+    document.getElementById(input).style.backgroundColor = 'red';
+
+    if(document.getElementById('87Gas').style.backgroundColor === 'red');
+    {
+        return;
+    }
+
+    document.getElementById(input).style.backgroundColor = 'red';
+    document.getElementById('89Gas').style.backgroundColor = 'red';
+    document.getElementById('93Gas').style.backgroundColor = 'red';
+    document.getElementById('dieselGas').style.backgroundColor = 'red';
+}
+
+function addToEquation(input) {
+    document.getElementById('screen').innerHTML = input;
+}
+
+function deleteEntry() {
+    document.getElementById('screen').innerHTML = 'SCREEN';
+}
---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 9000
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'htmlCss/index.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
