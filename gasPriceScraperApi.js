2a0672fd17c52d72d2c5d9d615cefab442f9c8e2
pdurfey <3durfey@gmail.com>
2023-02-13 22:51:32 -0500

    change folder name

--- gasPriceScraperApi.js
+++ gasPriceScraperApi.js
@@ -2,11 +2,9 @@ import gotScraping from 'got-scraping';
 import cheerio from 'cheerio';
 
 const storeUrl = 'https://api.apify.com/v2/datasets/oRfOLi79S4TcYn7V7/items?token=apify_api_DVgZweSpe7Lp9jEr9AQht1ERabqQER1sxBgR';
-
 // Download HTML with Got Scraping
 const response = await gotScraping.gotScraping(storeUrl);
 const html = response.body;
-
 // Parse HTML with Cheerio
 const $ = cheerio.load(html);
 const headingElement = $('gasPrices');
@@ -15,3 +13,5 @@ const jsonFile = JSON.parse(html);
 let dataSet = jsonFile[9];
 // Print page title to terminal
 console.log(dataSet.gasPrices[0].priceTag);
+
+
