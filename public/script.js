864e6db7a24be33c5f3b62e1de5e690042e88275
pdurfey <3durfey@gmail.com>
2023-02-18 20:30:49 -0500

    work on cash functions

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 let chosenGasBool = false; 
 let chosenGasNumber;
 let chosenGasPrice = 0;
-let paymentMethod = 'none';
+let paymentMethod;
 let paymentMethodBool = false;
 let inputField = document.getElementById('interface');
 let decimalField = document.getElementById('decimal');
@@ -10,156 +10,152 @@ let beginFuelingButton = document.getElementById('beginFueling');
     beginFuelingButton.disabled = true;
 let currentInputNumber = '';
 let cashAmount = 0;
-let gasTankSize;
+let gasTankSize = 0;
 let amountOfGas;
 let decimalBool = false;
+let stepInPumpProcess = 0;
+let costOfGas = 0;
 
+//disable unused buttons
+document.getElementById('D').disabled = true;
+document.getElementById('no').disabled = true;
+document.getElementById('yes').disabled = true;
 
-//change color of button for chosen gas type
-function changeColor(input, inputedPrice)
-{
-    let price=document.getElementById(inputedPrice).innerHTML;
-    price=price.replace('$', '');
-    let element=document.getElementById(input);
-    if(!chosenGasBool && paymentMethod != 'none')
-    {
-        element.style.backgroundColor = "red";
-        chosenGasBool = true;
-        chosenGasNumber = input;
-        chosenGasPrice = price;
-        console.log(chosenGasNumber, " ", price);
-    }
-}
-//reset everything for new transaction
-function reset()
-{
-    chosenGasBool = false;
-    chosenGasPrice = 0;
-    paymentMethod = 'none';
-    paymentMethodBool = false;
-    currentInputNumber = '';
-    cashAmount = 0;
-    gasTankSize = 0;
-    gallonsPumped = 0;
-    gallonsInput.innerHTML = 'Gallons: 0';
-    decimalField.innerHTML = '';
-    if(chosenGasNumber)
-    {
-        let element=document.getElementById(chosenGasNumber);
-        element.style.backgroundColor = "white";
-    }
-    chosenGasNumber = '';
-    paymentMethod = 'none';
-    inputField.innerHTML = 'Enter 1 for credit or 2 for cash';
-}
-//determine payment type
+//determine payment type, 1st step in process
 function paymentMethodFunction(input)
 {
     let message;
-    let inputValue;
-    
-    if(input === 'credit')
-    {
-        message = "input credit card number and press ENTER";
-        inputValue = '1';
-    }
-    else
-    {
-        message = "input cash amount and press ENTER";
-        inputValue = '2';
-    }
-    if(paymentMethod === 'none')
-    {
-        paymentMethod = input;
-        inputField.innerHTML = message;
-    }
-    else
-    {
-        addToInput(inputValue);
-    }
-}
-//add input to current input
-function addToInput(input) 
-{
-    currentInputNumber += input;
-    inputField.innerHTML = currentInputNumber;
-}
-
-function deleteEntry() {
-    document.getElementById('screen').innerHTML = 'SCREEN';
-}
-
-function compute()
-{
-    if(!paymentMethodBool)
-    {
-        if(paymentMethod === 'credit')
-        {   
-            if(currentInputNumber.length > 4 || currentInputNumber.length < 4)
-            {
-                inputField.innerHTML = 'Invalid credit card info';
-            }
-            else
-            {
-                inputField.innerHTML = 'Enter size of gas tank and press ENTER';
-            }
-        }
-        else if(paymentMethod === 'cash')
-        {
-            cashAmount = currentInputNumber;
-        }
-        paymentMethodBool = true;
-        currentInputNumber = '';
+    if(stepInPumpProcess >= 3) {
         return;
     }
-    else if(!gasTankSize && paymentMethod != 'cash')
-    {
-        gasTankSize = currentInputNumber;
-        if(!chosenGasNumber)
+    if(stepInPumpProcess != 0) {
+        addToInput(input);
+        return;
+    }
+    else if(input === 1) {
+        message = "input credit card number and press ENTER";
+        paymentMethod = 'card';
+    }
+    else {
+        message = "input cash amount and press ENTER";
+        paymentMethod = 'cash';
+    }
+    inputField.innerHTML = message;
+    stepInPumpProcess++;
+}
+//2nd step in process
+function gallonsOrDollars() {
+    if (paymentMethod === 'card') {
+        if(currentInputNumber.length > 4 || currentInputNumber.length < 4)
         {
-            inputField.innerHTML = 'Select Gas type and press ENTER';
+            inputField.innerHTML = 'Invalid credit card info';
+            return;
+        } else {
+            inputField.innerHTML = 'Enter size of gas tank and press ENTER';
+        } 
+    } else if(paymentMethod === 'cash') {
+        cashAmount = currentInputNumber;
+        if(currentInputNumber === '') {
             return;
         }
+        inputField.innerHTML = `$${cashAmount} of gas purchased, choose gas type`;
     }
-    else if(!chosenGasNumber)
-    {
-        inputField.innerHTML = 'Choose gas type and press ENTER';
+    currentInputNumber = '';
+    stepInPumpProcess++;
+}
+//3rd step in process
+function tankSize() {
+    if(currentInputNumber <= 0) {
         return;
     }
-    if(paymentMethod === 'cash')
-    {
-        amountOfGas = cashAmount / chosenGasPrice;
-        gasTankSize = Math.floor(Math.random() * (amountOfGas - 3) + 3);
-        inputField.innerHTML = Math.round(amountOfGas * 100)/100 +' gallons purchased';
-        beginFuelingButton.disabled = false;
+    if(paymentMethod === 'card') {
+        gasTankSize = currentInputNumber;
+        inputField.innerHTML = 'Chose type of gas';
+
     }
-    else if(paymentMethod === 'credit')
-    {
-        amountOfGas = gasTankSize * chosenGasPrice;
-        inputField.innerHTML = Math.round(amountOfGas * 100)/100 + ' dollars of gas purchased';
-        beginFuelingButton.disabled = false;
+    stepInPumpProcess++;
+}
+
+//change color of button for chosen gas type 4th step
+function changeColor(input, inputedPrice) {
+    let price = document.getElementById(inputedPrice).innerHTML;
+    price = price.replace('$', '');
+    let ChosenButton = document.getElementById(input);
+    if(!chosenGasBool && (gasTankSize != 0 || cashAmount != 0)) {
+        ChosenButton.style.backgroundColor = "red";
+        chosenGasBool = true;
+        chosenGasNumber = input;
+        chosenGasPrice = price;
+        stepInPumpProcess++;
+    } 
+}
+//compute amount of gas to be pumped 5th step
+function amountOfGasPumped() {
+    if(paymentMethod === 'cash') {
+        amountOfGasPumped = cashAmount / chosenGasPrice;
+        gasTankSize = amountOfGasPumped;
+        inputField.innerHTML = Math.round(amountOfGasPumped * 100)/100 +' gallons purchased press BEGIN FUELING';
+    } else if(paymentMethod === 'card') {
+        costOfGas = gasTankSize * chosenGasPrice;
+        inputField.innerHTML = Math.round(costOfGas * 100)/100 + ' dollars of gas purchased press BEGIN FUELING';
+    }
+    stepInPumpProcess++;
+    beginFuelingButton.disabled = false;
+}
+
+//add input to current input
+function addToInput(input) {
+    if(stepInPumpProcess >= 3 || cashAmount > 0) {
+        return;
+    }
+    if(stepInPumpProcess != 0) {
+        currentInputNumber += input;
+        inputField.innerHTML = currentInputNumber;
     }
 }
+//switch statement for process
+function compute() {   
+    switch(stepInPumpProcess) {
+    case 1:  
+        gallonsOrDollars();
+        break;
+    case 2:
+        tankSize();
+        break;
+    case 3:
+        amountOfGasPumped();
+        break;
+    case 4:
+        amountOfGasPumped();
+        break;
+    }
+}
+
+
 
 // variables for interval timer
 let gallonsPumped = 0;
 let intervalId;
 let decIntervalId;
 let decimal = 0;
+let gallonBool = false;
 //show gallons as they are being pumped
 function showGallons() {
    inputField.innerHTML = "NOW FUELING";
    intervalId =  setInterval(incrementGallons, 2000);
    decimalBool = false;
+   console.log('tank size ' + gasTankSize)
 }
 //increment gallons
 function incrementGallons() {
-    if (gallonsPumped <= gasTankSize) {
+    if (gallonsPumped <= gasTankSize && gallonBool === false) {
         gallonsInput.innerHTML = `Gallons: ${gallonsPumped}`;
         gallonsPumped += 1;
         decIntervalId = setInterval(incrementDecimal, 200);
     } else {
+        console.log('end')
         return clearInterval(intervalId);
+
         // inputField.innerHTML = 'Thank you! Would you like a reciept?';
     }
 }
@@ -168,22 +164,31 @@ function incrementDecimal() {
     //if gallons is less than 10 increment decimal, otherwise set to 0 and return
     if (decimal < 10 && decimalBool === false) {
        decimalField.innerHTML = `.${decimal}`;
-       decimal += 1;
+        //if gallons pumped has reached amount needed return and clear interval 
+        let totalGallons = gallonsPumped + decimal * .1 + 1;   
+  
+        console.log('total ' + totalGallons)
+
+        if(totalGallons > gasTankSize) {
+            console.log('total gallons pumped ' + totalGallons);
+            decimalBool = true;
+            gallonbool = true;
+            decimalField.innerHTML = `.${decimal}`;
+            return clearInterval(decIntervalId);
+        }
+        decimal++;
    }
    else {
-      decimal = 0;
+      if(gallonBool != true) {
+        decimal = 0;
+      }
       decimalField.innerHTML = `.${decimal}`;
+      console.log('end decimal')
       return clearInterval(decIntervalId);
    } 
-   //if gallons pumped has reached amount needed return and clear interval   
-   if(gallonsPumped > gasTankSize) {
-       decimalBool = true;
-       return clearInterval(decIntervalId);
-   }
 }
 
 //frontend for getting data for gas prices API
-
 let gasPrice87 = document.getElementById('price87');
 let gasPrice89 = document.getElementById('price89');
 let gasPrice93 = document.getElementById('price93');
@@ -202,4 +207,27 @@ async function getPrices() {
     gasPrice89.innerHTML = data[1];
     gasPrice93.innerHTML = data[2];
     gasPriceDiesel.innerHTML = data[3];
+}
+
+//reset everything for new transaction
+function reset()
+{
+    chosenGasBool = false;
+    chosenGasPrice = 0;
+    paymentMethod = 'none';
+    paymentMethodBool = false;
+    currentInputNumber = '';
+    cashAmount = 0;
+    gasTankSize = 0;
+    gallonsPumped = 0;
+    gallonsInput.innerHTML = 'Gallons: 0';
+    decimalField.innerHTML = '.00';
+    if(chosenGasNumber)
+    {
+        let element=document.getElementById(chosenGasNumber);
+        element.style.backgroundColor = "white";
+    }
+    chosenGasNumber = '';
+    paymentMethod = 'none';
+    inputField.innerHTML = 'Enter 1 for credit or 2 for cash';
 }
