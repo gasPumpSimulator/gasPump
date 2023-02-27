d6375878b240caf817d35223828c591189bb3268
pdurfey <3durfey@gmail.com>
2023-02-27 16:12:58 -0500

    ssh working

--- ssh.js
+++ ssh.js
@@ -1,44 +1,142 @@
 import { Client } from 'ssh2';
-import express, { json } from 'express'
-import path from 'path'
-import { fileURLToPath } from 'url';
-import { getTransactions, getTransaction, createTransaction } from './database.js';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const port = 3000;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const app = express()
-
-app.use(express.json());
-
-
-
-
-
-
+let connection = function(){};
+/*
+function createDBConnection() {
+	let mysqlConnection = mysql.createConnection({
+		host: process.env.MYSQL_HOST,
+		user: process.env.MYSQL_USER,
+		password: process.env.MYSQL_PASSWORD,
+		database: process.env.MYSQL_DATABASE,
+	});
+	return mysqlConnection;
+};*/
 
 
+	const ssh = new Client();
+	ssh.connect({
+		host: '158.101.103.144',
+    	username: 'opc',
+    	privateKey: process.env.SSH_privatekey,
+	});
+
+	ssh.on('ready', function() {
+		ssh.forwardOut(
+			'127.0.0.1', // any valid address
+			12345, // any valid port
+			'10.11.10.77', // destination database
+			3306, // destination port
+			function(err, stream) {
+				if (err) console.log('ssh 1 error')
+				
+				let  config = 
+				{
+					host: process.env.MYSQL_HOST,
+					user: process.env.MYSQL_USER,
+					password: process.env.MYSQL_PASSWORD,
+					database: process.env.MYSQL_DATABASE,
+					stream
+				};
+
+				let con = mysql.createConnection(config);
+				con.connect(function(err) {
+					if (err) throw err;
+					else
+					console.log("Connected!");
+				  });
+				/*
+				db.query("Select * from Transactions", function(err, rows){
+					if(rows) {
+						console.log(rows);
+						data(rows);
+					}
+					if(err) { handleMySQLError(err)}
+				})*/
+			}
+		);
+	});
 
 
-console.log(process.env.SSH_privatekey);
 
 
 
-const conn = new Client();
-conn.on('ready', () => {
+
+
+
+
+/*
+const forwardConfig = {
+    srcHost: '127.0.0.1', // any valid address
+    srcPort: 3306, // any valid port
+    dstHost: '10.11.10.77', // destination database
+    dstPort: 3306 // destination port
+};
+const sshClient = new Client();
+const SSHConnection = new Promise((resolve, reject) => {
+    sshClient.on('ready', () => {
+		sshClient.forwardOut(
+		'158.101.103.144',
+		'opc',
+		process.env.SSH_privatekey,
+        (err, stream) => {
+             if (err) reject(err);
+           
+            // create a new DB server object including stream
+            const updatedDbServer = {
+                 ...dbServer,
+                 stream
+            };
+            // connect to mysql
+            const connection =  mysql.createConnection(updatedDbServer);
+            // check for successful connection
+           //  resolve or reject the Promise accordingly          
+           connection.connect((error) => {
+            if (error) {
+				console.log('error test');
+                reject(error);
+            }
+			else {
+				console.log('success')
+			}
+            resolve(connection);
+            });
+       });
+}).connect({
+	host: '158.101.103.144',
+    username: 'opc',
+    privateKey: process.env.SSH_privatekey,
+});
+});
+
+
+create new client
+const sshConnection = new Client();
+
+sshConnection.on('ready', () => {
   console.log('Client :: ready');
-  conn.shell((err, stream) => {
+  sshConnection.shell((err, stream) => {
     if (err) throw err;
+
+	//closing stream
     stream.on('close', () => {
       console.log('Stream :: close');
-      conn.end();
+      sshConnection.end();
+	
     }).on('data', (data) => {
-      console.log('OUTPUT: ' + data);
+      console.log('data entered: ' + data);
     });
     stream.end('ls -l\nexit\n');
+
+
   });
 }).connect({
     host: '158.101.103.144',
     username: 'opc',
     privateKey: process.env.SSH_privatekey,
 });
+*/
+
+
