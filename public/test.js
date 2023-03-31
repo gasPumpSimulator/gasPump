4f594b33f15c3c0096066e3ec6cbf31bc4ead868
pdurfey <3durfey@gmail.com>
2023-03-30 20:03:28 -0400

    added object for variables

---
+++ public/test.js
@@ -0,0 +1,17 @@
+let transactionObject = {
+  chosenGasNumber: "",
+  chosenGasPrice: "",
+  paymentMethod: "",
+  cashAmount: "",
+  gasTankSize: "",
+  // eslint-disable-next-line no-dupe-keys
+  cashAmount: "",
+  costOfGas: "",
+  creditCardNumber: "",
+  creditCardName: "",
+  cvcCode: "",
+  creditExp: "",
+};
+
+transactionObject.chosenGasNumber = 3333;
+console.log(transactionObject.chosenGasNumber);
